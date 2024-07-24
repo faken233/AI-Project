0759ff435f3333558de953c1ff8702c85f79a56b
Xb2555 <2012753288@qq.com>
2024-07-24 13:02:15 +0800

    refactor:啥也没做

--- src/main/java/com/faken/aiproject/constant/Constant.java
+++ src/main/java/com/faken/aiproject/constant/Constant.java
@@ -3,4 +3,6 @@ package com.faken.aiproject.constant;
 public class Constant {
     public static final String SECURE_KEY = "HUAWEIALIBABA";
     public static final String adminId = "admin";
+
+
 }
--- src/main/java/com/faken/aiproject/mapper/AccountMapper.java
+++ src/main/java/com/faken/aiproject/mapper/AccountMapper.java
@@ -3,10 +3,7 @@ package com.faken.aiproject.mapper;
 import com.faken.aiproject.po.dto.LoginDTO;
 import com.faken.aiproject.po.dto.RegisterDTO;
 import com.faken.aiproject.po.entity.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface AccountMapper {
@@ -20,4 +17,6 @@ public interface AccountMapper {
     @Options(useGeneratedKeys = true, keyProperty = "userId")
     @Insert("insert into users (password, email, user_name) VALUES (#{password}, #{email}, #{userName})")
     void registerNewUser(User newUser);
+
+
 }

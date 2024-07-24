0e67b529b4b9c6aae2d1a055520439f929b70a5f
Suyyyus <2893796696@qq.com>
2024-07-24 11:52:35 +0800

    feat: 初步设计实体类

---
+++ src/main/java/com/faken/aiproject/po/entity/Model.java
@@ -0,0 +1,24 @@
+package com.faken.aiproject.po.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Model {
+       private int modelId; //主键ID
+       private String modelName; //模型名称
+       private int userId; //用户ID
+       private int characterType; // 官方/用户
+       private int modelType; // 图形/文本
+       private String version; //版本
+       private String description; //描述
+       private int usedTimes; //使用次数
+       private Date createTime; //上传时间
+
+
+}
---
+++ src/main/java/com/faken/aiproject/po/entity/ModelUrl.java
@@ -0,0 +1,16 @@
+package com.faken.aiproject.po.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelUrl {
+   private int modelUrlId; //主键ID
+   private int modelId; //模型名称
+   private String url; //模型文件地址
+
+
+}

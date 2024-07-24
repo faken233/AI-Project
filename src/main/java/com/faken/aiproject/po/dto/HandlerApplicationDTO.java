26aa451c2c480287979c0aa64a4997cb3f984ba7
Suyyyus <2893796696@qq.com>
2024-07-24 23:29:46 +0800

    feat: 新增DTO

---
+++ src/main/java/com/faken/aiproject/po/dto/HandlerApplicationDTO.java
@@ -0,0 +1,13 @@
+package com.faken.aiproject.po.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HandlerApplicationDTO {
+
+    private int applicationId;
+}
